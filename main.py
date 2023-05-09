25b1168529670e1a73b44f9ab2aa8399416b8dad
sfysfy <sfy849015575@outlook.com>
2023-05-09 17:57:48 +0800

    Update

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import cv2
+from ultralytics import YOLO
 
-from utils.dataloaders import LoadStreams
-
+from utils.dataloaders import LoadRTSPs
+from utils.common import LOGGER
 
 rtsp_urls = [
-    'rtsp://admin:nike@2021@117.85.174.233:1554/video0',
-    'rtsp://admin:nike@2021@117.85.174.233:1557/video0'
+    'rtsp://admin:akd123456@192.168.1.168/Streaming/Channels/101',
+    'rtsp://admin:akd123456@192.168.1.168/Streaming/Channels/201',
+    'rtsp://admin:akd123456@192.168.1.168/Streaming/Channels/301',
+    'rtsp://admin:akd123456@192.168.1.168/Streaming/Channels/401',
 ]
 
-dataloader = LoadStreams(rtsp_urls)
-for imgs in dataloader:
-    for i, img in enumerate(imgs):
-        cv2.imshow(str(i), img)
-        if cv2.waitKey(1) == ord('q'):
-            cv2.destroyAllWindows()
-            exit()
+model = YOLO('weights/yolov8n.pt')
+dataloader = LoadRTSPs(rtsp_urls, skip_frame=250, show_log=False)
+model(dataloader.imgs)
+# for i in range(len(rtsp_urls)):
+#     cv2.namedWindow(str(i), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
 
+for frames in dataloader:
+    imgs = []
+    for i, frame in enumerate(frames):
+        log, img = frame
+        imgs.append(img)
+        # cv2.imshow(str(i), img)
+        LOGGER.info(f"{i}:{log['qsize']:>4},{log['n']:>5},{log['dt']:>6.2f},{log['cv_msec']:>9.2f}")
+    results = model(imgs)
+    # for i, result in enumerate(results):
+    #     annotated_frame = result.plot()
+    #     cv2.imshow(str(i), annotated_frame)
+    #
+    # if cv2.waitKey(1) == ord('q'):
+    #     cv2.destroyAllWindows()
+    #     exit()
--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -3,49 +3,67 @@ import cv2
 import math
 import numpy as np
 from threading import Thread
+import queue
 
 from utils.common import StatTime, LOGGER
 
 
-class LoadStreams:
-    def __init__(self, sources, vid_stride=1):
+class LoadRTSPs:
+    def __init__(self, sources, vid_stride=1, skip_frame=125, show_log=True):
         self.sources = [sources] if isinstance(sources, str) else sources
         self.vid_stride = vid_stride
+        self.skip_frame = skip_frame
+        self.show_log = show_log
+        self.reset = True
 
         n = len(self.sources)
-        self.imgs, self.fps, self.frames, self.threads = [None] * n, [0] * n, [0] * n, [None] * n
+        self.imgs, self.fps, self.frames, self.threads = [None] * n, [0] * n, [], [None] * n
+        self.dts = [StatTime()] * n
+        # 初始化
         for i, s in enumerate(sources):
             st = f'{i + 1}/{n}: {s}... '
-            s = eval(s) if s.isnumeric() else s
             cap = cv2.VideoCapture(s)
             assert cap.isOpened(), f'{st}Failed to open {s}'
 
             w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             fps = cap.get(cv2.CAP_PROP_FPS)
-            self.frames[i] = max(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), 0) or float('inf')
             self.fps[i] = max((fps if math.isfinite(fps) else 0) % 100, 0) or 30
 
-            _, self.imgs[i] = cap.read()  # guarantee first frame
+            _, self.imgs[i] = cap.read()
             self.threads[i] = Thread(target=self.update, args=([i, cap, s]), daemon=True)
-            LOGGER.info(f'{st} Success ({self.frames[i]} frames {w}x{h} at {self.fps[i]:.2f} FPS)')
+            LOGGER.info(f'{st} Success ({w}x{h} at {self.fps[i]:.2f} FPS)')
+            self.frames.append(queue.Queue())
+
+        for i in range(n):
             self.threads[i].start()
-        LOGGER.info('')
 
     def update(self, i, cap, stream):
-        n, f = 0, self.frames[i]
-        while cap.isOpened() and n < f:
+        n, fq, dt = 0, self.frames[i], self.dts[i]
+        while cap.isOpened() and n < self.skip_frame and self.reset:
             n += 1
             cap.grab()
-            if n % self.vid_stride == 0:
-                success, img = cap.retrieve()
-                if success:
-                    self.imgs[i] = img
-                else:
-                    LOGGER.warning('WARNING ⚠️ Video stream unresponsive, please check your IP camera connection.')
-                    self.imgs[i] = np.zeros_like(self.imgs[i])
-                    cap.open(stream)
             time.sleep(0.0)
+
+        self.reset = False
+        n = 0
+        while cap.isOpened():
+            with dt:
+                n += 1
+                cap.grab()
+                if n % self.vid_stride == 0:
+                    success, img = cap.retrieve()
+                    msec = cap.get(cv2.CAP_PROP_POS_MSEC)
+                    if success:
+                        self.imgs[i] = img
+                    else:
+                        LOGGER.warning('WARNING ⚠️ Video stream unresponsive, please check your IP camera connection.')
+                        self.imgs[i] = np.zeros_like(self.imgs[i])
+                        cap.open(stream)
+                time.sleep(0.0)
+            fq.put([{'n': n, 'dt': dt.dt, 'cv_msec': msec}, self.imgs[i]])
+            if self.show_log:
+                LOGGER.info(f"update {i}: {n:>4}, {dt.dt:>6.2f}, {msec:>8.2f}")
 
     def __iter__(self):
         self.count = -1
@@ -55,9 +73,16 @@ class LoadStreams:
         return len(self.sources)
 
     def __next__(self):
-        self.count += 1
-        if not all(x.is_alive() for x in self.threads) or cv2.waitKey(1) == ord('q'):  # q to quit
-            cv2.destroyAllWindows()
-            raise StopIteration
-        return self.imgs
+        while True:
+            frames_qsize = []
+            for fq in self.frames:
+                frames_qsize.append(fq.qsize())
+            if (np.array(frames_qsize) > 0).all():
+                break
 
+        frames = []
+        for i, fq in enumerate(self.frames):
+            frame = fq.get()
+            frame[0]['qsize'] = frames_qsize[i]
+            frames.append(frame)
+        return frames
